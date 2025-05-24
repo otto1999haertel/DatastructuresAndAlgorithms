80f3faf44afaf6297cdb9acd9396e82ea2c23ef9
ottohartel <otto.haertel@gmail.com>
2025-05-24 11:33:00 +0200

    set up symmetric tree tests

--- TreeSymmetric/TreeSymmetricLib/TreeNode.cs
+++ TreeSymmetric/TreeSymmetricLib/TreeNode.cs
@@ -6,9 +6,15 @@ public class TreeNode
     public TreeNode left;
     public TreeNode right;
 
-    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null) {
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    public bool IsSymmetric(TreeNode startNode)
+    {
+        return false;
     }
 }
---
+++ TreeSymmetric/TreeSymmetricTest/TreeSymmetricTest.cs
@@ -0,0 +1,54 @@
+﻿namespace TreeSymmetricTests;
+
+using TreeSymmetricLib;
+
+public class TreeSymmetricTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void SymmetricTree_ReturnsTrue()
+    {
+        var tree = new TreeNode(1,
+            new TreeNode(2, new TreeNode(3), new TreeNode(4)),
+            new TreeNode(2, new TreeNode(4), new TreeNode(3))
+        );
+
+        var result = tree.IsSymmetric(tree);
+        Assert.That(result.Equals(true));
+    }
+
+    [Test]
+    public void AsymmetricTree_ReturnsFalse()
+    {
+        var tree = new TreeNode(1,
+            new TreeNode(2, null, new TreeNode(3)),
+            new TreeNode(2, null, new TreeNode(3))
+        );
+
+        var result = tree.IsSymmetric(tree);
+        Assert.That(result.Equals(false));
+    }
+
+    [Test]
+    public void EmptyTree_ReturnsTrue()
+    {
+        TreeNode? tree = null;
+
+        var result = tree.IsSymmetric(tree);
+        Assert.That(result.Equals(null));
+    }
+
+    [Test]
+    public void SingleNodeTree_ReturnsTrue()
+    {
+        var tree = new TreeNode(1);
+
+        var result = tree.IsSymmetric(tree);
+        Assert.That(result.Equals(true));
+    }
+}
+
