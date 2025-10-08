ecf505e3e8665f2eb314294ac3051fdcb0ea8ea5
Haertel <otto.haertel@siemens.com>
2025-10-08 17:39:12 +0200

    todo: implement parallel programming fibonacci with tasks

---
+++ Threading/ThreadingImpl/FibonacciCalculator.cs
@@ -0,0 +1,6 @@
+namespace ThreadingImpl;
+//TODO: Fibonacci Zahlen im angeben Zahlenbereich von 1 bis n parallel berechnen durch Tasks in angabe von sections
+//Thread sichere Sammlung der Ergebnisse (z.B. ConcurrentBag)
+public class FibonacciCalculator
+{
+}
