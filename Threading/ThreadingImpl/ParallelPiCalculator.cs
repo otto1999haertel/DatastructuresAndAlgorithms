61f928b35050936aa9647e1d6208d30bd2642fc9
ottohartel <otto.haertel@gmail.com>
2025-10-08 19:00:55 +0200

    refactor: changing name and coding challenge

---
+++ Threading/ThreadingImpl/ParallelPiCalculator.cs
@@ -0,0 +1,9 @@
+namespace ThreadingImpl;
+//TODO: Aufgabe: Parallele Pi-Berechnung mit der Monte-Carlo-Methode
+//Daten zufällig in Quadtrat generierte 
+//Teile die Gesamtzahl der Punkte in Batches auf (z. B. 10 Batches à 1 Million Punkte).
+//Aggregiere die hits aus allen Batches und berechne Pi: π ≈ 4 * sum(hits) / total
+//Lernziel ParallelForEach
+public class ParallelPiCalculator
+{
+}
