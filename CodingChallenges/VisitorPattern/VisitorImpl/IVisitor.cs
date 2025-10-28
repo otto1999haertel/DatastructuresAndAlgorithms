5eb3464755d99ada6a106cd49c7df932ceab344f
Haertel <otto.haertel@siemens.com>
2025-10-28 15:54:16 +0100

    chore: adapt commenting

--- CodingChallenges/VisitorPattern/VisitorImpl/IVisitor.cs
+++ CodingChallenges/VisitorPattern/VisitorImpl/IVisitor.cs
@@ -1,6 +1,6 @@
 public interface IVisitor<T>
 {
     //defines what type of expression the visitor supports
-    //Operation, die auf Element ausgeführt wird
+    //Operation/ Algorithmus, die auf Element ausgeführt wird
     T VisitAdder(Adder adder);
 }
